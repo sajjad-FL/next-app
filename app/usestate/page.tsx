fba48e7b787a6a4af785fb11d1ed2ca718ea22c3
sajjad-FL <101242343+sajjad-FL@users.noreply.github.com>
2023-07-20 00:49:37 +0530

    use state in typescript with next js completed

---
+++ app/usestate/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+import React, { useState } from 'react'
+
+interface IUser {
+    id: number,
+    name: string,
+    password: string
+}
+
+function Logged(): JSX.Element {
+
+  const [name, setName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [details, setDetails] = useState<IUser | null>(null)
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+     setName(e.target.value);
+  }
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  }
+
+  const handleLogin = () => {
+    setDetails({id: Math.random(), name, password});
+  }
+
+  details && alert(JSON.stringify({details}));
+  return (
+    <div>
+      {
+        details ? `You are looged In ${name}` :  <div>
+        <div className='looded'>Login</div>
+       <input type="text" placeholder='enter email' onChange={handleNameChange} />
+       <input type="text" placeholder='enter password' onChange={handlePasswordChange} />
+       <button onClick={handleLogin}> Submit </button>
+  </div>
+      }
+    </div>
+  )
+}
+
+export default Logged;
