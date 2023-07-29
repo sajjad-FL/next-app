e5d82f61363ec1b55621aab822496c53d6de7312
sajjad-FL <101242343+sajjad-FL@users.noreply.github.com>
2023-07-29 14:02:23 +0530

    loading page added to the component level

--- app/scalls/page.tsx
+++ app/scalls/page.tsx
@@ -1,22 +1,21 @@
 import axios, {AxiosResponse, AxiosError} from 'axios'
 import React from 'react'
 import User from '../components/User';
-import { IUsers } from '../types/types';
 import { emailQueue } from '../api/queues/route';
 
 const getRes = async () => {
   const url: string = 'https://jsonplaceholder.typicode.com/todos'
-  const res: AxiosResponse<IUsers[]> = await axios.get(url);
+  const res: AxiosResponse<any[]> = await axios.get(url);
   return res.data;
 }
 
 async function Scalls() {
-  const data: IUsers[] = await getRes();
+  const data: any[] = await getRes();
 
   return (
     <div className='flex min-h-screen flex-col items-center justify-between p-24'>Parent
     {
-      data?.map((user: IUsers) => (
+      data?.map((user: any) => (
         <>
         <User key={user.id} {...user} />
         </>
